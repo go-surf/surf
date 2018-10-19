12ff4ca37de3438e78588b6fc872d82992b30997
Piotr Husiatyński <phusiatynski@gmail.com>
2018-10-19 10:44:47 +0200

    JSONResp returns a surf.Response instead of directly writing content

--- json_response.go
+++ json_response.go
@@ -1,29 +1,47 @@
 package surf
 
 import (
+	"bytes"
 	"encoding/json"
+	"io"
 	"net/http"
+	"strings"
 )
 
 // JSONResp write content as JSON encoded response.
-func JSONResp(w http.ResponseWriter, code int, content interface{}) {
+func JSONResp(code int, content interface{}) Response {
 	b, err := json.MarshalIndent(content, "", "\t")
 	if err != nil {
-		code = http.StatusInternalServerError
-		b = []byte(`{"errors":["Internal Server Errror"]}`)
+		return &jsonResponse{
+			code: http.StatusInternalServerError,
+			body: strings.NewReader(`{"errors":["Internal Server Errror"]}`),
+		}
 	}
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(code)
-	w.Write(b)
+	return &jsonResponse{
+		code: code,
+		body: bytes.NewReader(b),
+	}
+}
+
+type jsonResponse struct {
+	code int
+	body io.Reader
+}
+
+func (resp *jsonResponse) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	header := w.Header()
+	header.Set("content-type", "application/json; charset=utf-8")
+	w.WriteHeader(resp.code)
+	io.Copy(w, resp.body)
 }
 
 // JSONErr write single error as JSON encoded response.
-func JSONErr(w http.ResponseWriter, code int, errText string) {
-	JSONErrs(w, code, []string{errText})
+func JSONErr(code int, errText string) {
+	JSONErrs(code, []string{errText})
 }
 
 // JSONErrs write multiple errors as JSON encoded response.
-func JSONErrs(w http.ResponseWriter, code int, errs []string) {
+func JSONErrs(code int, errs []string) {
 	resp := struct {
 		Code   int      `json:"code"`
 		Errors []string `json:"errors"`
@@ -31,16 +49,16 @@ func JSONErrs(w http.ResponseWriter, code int, errs []string) {
 		Code:   code,
 		Errors: errs,
 	}
-	JSONResp(w, code, resp)
+	JSONResp(code, resp)
 }
 
 // StdJSONResp write JSON encoded, standard HTTP response text for given status
 // code. Depending on status, either error or successful response format is
 // used.
-func StdJSONResp(w http.ResponseWriter, code int) {
+func StdJSONResp(code int) {
 	if code >= 400 {
-		JSONErr(w, code, http.StatusText(code))
+		JSONErr(code, http.StatusText(code))
 	} else {
-		JSONResp(w, code, http.StatusText(code))
+		JSONResp(code, http.StatusText(code))
 	}
 }
